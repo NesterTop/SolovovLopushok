f82d301c784138da2c3e907bace29001a5a59b85
Андрей Соловов <solovov2003@yandex.ru>
2023-04-09 19:20:34 +0300

    Библеотека

---
+++ WSUniversalLib/Calculation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSUniversalLib
+{
+    public class Calculation
+    {
+        public int GetQuantityForProduct(int productType, int materialType, int count, float width, float length)
+        {
+            float p = width * length;
+            float result = p * productType + materialType;
+
+            return (int)result;
+        }
+    }
+}
